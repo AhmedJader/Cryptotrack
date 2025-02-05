1e9d1f89099b6a5488f83af9d16d1e8b56ed515d
Ahmed <ahmedjader837@gmail.com>
2025-02-05 13:08:21 -0500

    updated dashboard to have structured content, and call on crypto handle

--- app/pages/dashboard/page.tsx
+++ app/pages/dashboard/page.tsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import { UserButton } from '@clerk/nextjs'
-import User from '../../comp/userhandle'
+"use client";
 
+import React from "react";
+import { UserButton } from "@clerk/nextjs";//handles user profile info
+import User from "../../comp/userhandle"; //handles user info
+import CryptoHandle from "../../comp/cryptohandle"; //handles crypto api data
 const UserId = () => {
   return (
     <main>
-      <div className='flex min-h-screen flex-col items-center justify-center'>
-        <div className='fixed top-5 right-5'>
+      <div className="flex min-h-screen flex-col items-center justify-center">
+        {/* Header content */}
+        <div className="fixed top-5 right-5">
           <UserButton />
         </div>
+        <div className="fixed top-5 left-5">
+          <User />
+        </div>
 
-        <User />
+        {/* Crypto Portfolio */}
+        <CryptoHandle />
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default UserId
+export default UserId;
